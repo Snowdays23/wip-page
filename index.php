f9e5e4bb7eaa27f03a7f2d55a78a3fecafe57682
Francesco Mazzini <79516317+francescomazzini@users.noreply.github.com>
2022-11-09 09:53:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,37 +4,36 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		<link rel="icon" type="image/x-icon" href="img/snowflake_logo.png" alt="A" width="35" height="35">
-        <link rel= "stylesheet" href = "wip_style.css"/>
+        	<link rel= "stylesheet" href = "wip_style.css"/>
 	</head>
 	<body>
-     <div>
-            <div>
-                
-                <div >
-                    <img id="logo" src="img/Logo_SD_23.svg"  alt="logo" />
-                </div>
-                
-                <br>
-                
-                <div >
-                    <h1 class='h2'>WORK IN PROGRESS</h1>
-                </div>
+	     <div>
+		    <div>
+
+			<div >
+			    <img id="logo" src="img/Logo_SD_23.svg"  alt="logo" />
+			</div>
+
+			<br/>
+
+			<div>
+			    <h1 class='h2'>WORK IN PROGRESS</h1>
+			</div>
 
 
+			<div>
+			  <h2 class ='h3'> CAN'T WAIT TO WELCOME YOU IN BOZEN</h2>
+			</div>
 
-                <div>
-                  <h2 class ='h3'> CAN'T WAIT TO WELCOME YOU IN BOZEN</h2>
-                </div>
+		 	<div id = "load"></div>
 
-                 <div id = "load"></div>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
 
-                <br/>
-                <br/>
-                <br/>
-                <br/>
 
-                
-            </div>
-        </div>
-</body>
+		    </div>
+		</div>
+	</body>
 </html>	
