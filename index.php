5364bac30ff652ada63196c197a8af3e6ae9046e
cdercoli <105744782+cdercoli@users.noreply.github.com>
2022-11-09 09:05:56 +0100

    Added wip_page files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<html lang ="en">
+	<head>
+		<title>Snowdays23</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<link rel="icon" type="image/x-icon" href="img/snowflake_logo.png" alt="A" width="35" height="35">
+        <link rel= "stylesheet" href = "wip_style.css"/>
+	</head>
+	<body>
+     <div>
+            <div>
+                
+                <div >
+                    <img id="logo" src="img/Logo_SD_23.svg"  alt="logo" />
+                </div>
+                
+                <br>
+                
+                <div >
+                    <h1 class='h2'>WORK IN PROGRESS</h1>
+                </div>
+
+
+
+                <div>
+                  <h2 class ='h3'> CAN'T WAIT TO WELCOME YOU IN BOZEN</h2>
+                </div>
+
+                 <div id = "load"></div>
+
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+
+                
+            </div>
+        </div>
+</body>
+</html>	
